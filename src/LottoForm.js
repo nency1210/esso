ed613eb91bd0789af14ece12ec2ef425df9fb0ee
nency1210 <57886677+nency1210@users.noreply.github.com>
2024-07-24 20:07:35 -0400

    Update LottoForm.js

--- src/LottoForm.js
+++ src/LottoForm.js
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 const LottoForm = () => {
   const denominations = [2, 3, 5, 10, 20, 25, 30, 50];
   const currentDate = new Date();
-  const offset = currentDate.getTimezoneOffset() * 60000; // Get the timezone offset in milliseconds
-  const localDate = new Date(currentDate.getTime() - offset); // Adjust the current date based on the offset
+  const offset = currentDate.getTimezoneOffset() * 60000; 
+  const localDate = new Date(currentDate.getTime() - offset); 
 
   const initialFormData = {
     name: '',
@@ -39,24 +39,27 @@ const LottoForm = () => {
   useEffect(() => {
     localStorage.setItem('lottoFormData', JSON.stringify(formData));
   }, [formData]);
-
-  const handleChange = (e, denomination, type) => {
+  
+    const handleChange = (e, denomination, type) => {
     const value = parseFloat(e.target.value) || 0;
     const updatedFormData = { ...formData };
-
+  
     updatedFormData.lotto[denomination][type] = value;
-
+  
     if (type === 'add' || type === 'close') {
-      
-      updatedFormData.lotto[denomination].sold =
-        updatedFormData.lotto[denomination].add -
-        updatedFormData.lotto[denomination].close;
-      updatedFormData.lotto[denomination].dollar =
-        denomination * updatedFormData.lotto[denomination].sold;
+      if (updatedFormData.lotto[denomination].close === 0) {
+        updatedFormData.lotto[denomination].sold = 0;
+        updatedFormData.lotto[denomination].dollar = denomination * updatedFormData.lotto[denomination].add;
+      } else {
+        updatedFormData.lotto[denomination].sold =
+          updatedFormData.lotto[denomination].add -
+          updatedFormData.lotto[denomination].close;
+        updatedFormData.lotto[denomination].dollar =
+          denomination * updatedFormData.lotto[denomination].sold;
+      }
     }
-
     setFormData(updatedFormData);
-};
+  };
 const handleReset = () => {
     setFormData(initialFormData);
     localStorage.removeItem('lottoFormData');
@@ -65,7 +68,6 @@ const handleReset = () => {
 const handleSubmit = (e) => {
 e.preventDefault();
 console.log(formData);
-// You can perform any additional actions here, like submitting the data to a backend
 };
 
 const totalDollar = Object.values(formData.lotto)
